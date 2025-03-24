6a3c4e64ff5b2ac179cfbbd7f8d11cd74727f584
jonas01101111 <jonas1kiubi@gmail.com>
2025-03-24 15:56:55 -0300

    aula 12 minuto 33:06

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,10 +1,14 @@
-﻿namespace MauiAppMinhasCompras.Models
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        [PrimaryKey,AutoIncrement]
         public int Id { get; set; }
         public string Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco {  get; set; }
+        public double Total { get => Quantidade * Preco; }
     }
 }
--- MauiAppMinhasCompras/Views/ListarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListarProduto.xaml.cs
@@ -1,12 +1,23 @@
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListarProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListarProduto()
 	{
 		InitializeComponent();
+		lst_produtos.ItemsSource = lista;
 	}
 
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(x => lista.Add(x));
+    }
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
 		try
@@ -17,5 +28,22 @@ public partial class ListarProduto : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(x => lista.Add(x));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+		string msg = $"O total é {soma:C}";
+		DisplayAlert("Total dos produtos",msg, "OK");
     }
 }
